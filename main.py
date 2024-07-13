4985634d5fc6042d9b29b1caee56a2460628c1fe
Erik Kietzman <ErikKietzman@gmail.com>
2024-07-13 14:01:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to the Band Name Generator.")
+    city = input("Which city did you grow up in? \n")
+    pet = input("What's the name of your favorite pet? \n")
+    print(f'Your band name could be: "{city} {pet}!" \n')
+
+
+if __name__ == '__main__':
+    main()
